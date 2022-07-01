60caecf9d9edb2000f51fc0197dcfcf73ab5f3b1
grazirs <graziribeiro95@gmail.com>
2022-07-01 14:44:41 +0200

    add status component

---
+++ src/components/Status.tsx
@@ -0,0 +1,18 @@
+interface StatusProps {
+  status: "loading" | "success" | "error";
+}
+export const Status = (props: StatusProps) => {
+  let message;
+  if (props.status === "loading") {
+    message = "Loading...";
+  } else if (props.status === "success") {
+    message = "Data fetched successfully!";
+  } else if (props.status === "error") {
+    message = "Error fetching data";
+  }
+  return (
+    <div>
+      <h3>Status - {message}</h3>
+    </div>
+  );
+};
